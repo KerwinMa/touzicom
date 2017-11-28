25acf609f28c9a56ad09a79af06d6fa6afe738aa
yohance <1041255016@qq.com>
2017-11-28 17:54:47 +0800

    change

--- extend/test.php
+++ extend/test.php
@@ -1,4 +1,8 @@
 <?php
-require_once('./extend/CountUser.class.php');
-$CM = new CountUser();
+$redis = new redis();
+$redis->connect('127.0.0.1', 6379);
+$res = $redis->hGet('user_location',192);
+var_dump($res);
+$res = $redis->hSet('user_location',192,time());
+var_dump($res);
 ?>
